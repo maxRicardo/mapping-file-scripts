03f52b1291cccd76e7342b9fbd8849d9a4844498
maxRicardo <xaradrim@gmail.com>
2014-01-12 21:18:04 -0400

    Re-defintion of main function to make easier the proccess of testing and implementation of the parser envo.

--- mapping_enchancement/src/add_mapping_file_category.py
+++ mapping_enchancement/src/add_mapping_file_category.py
@@ -13,30 +13,19 @@ __version__= "0.1"
 #scrip to add field to mapping file
 
 from qiime.parse import parse_mapping_file as pmf
-from qiime.check_id_map import write_corrected_mapping as write_map
+#from qiime.check_id_map import write_corrected_mapping as write_map
 
 
 
-#==================== adding field function
+#==================== Script methods & Functions
 
-def add_field_to_map(field_fp,mapping_fp,separator=","):
+def add_field_to_map(field_fp,mapping_fp):
     #still some test needed in term of the
     #logic to stop posible errors to be raise
     #through execution.
     
-    #reading the column
-    field_file = open(field_fp,"r")
-    field_list = []
-    field_head = None
-    flag = True
-
-    #parsing the new field into list
-    for value in field_file.split(","):
-        if flag:
-            field_head = value
-            flag = False
-        else:
-            field_list.append(value)
+    #reading file with new column
+    mapping_cat,category = parse_category_from_file(field_fp)
         
     # Reading and parsing the mapping file
     mapp,head,comm = pmf(open(mapping_fp,"U"))
@@ -44,17 +33,50 @@ def add_field_to_map(field_fp,mapping_fp,separator=","):
     #adding field to headers
 
     temp = head.pop()
-    head.append(field_head)
+    head.append(category)
     head.append(temp)
 
     #adding field value in mapping
 
     for position in len(mapp):
         temp = mapp.pop()
-        mapp.append(field_list(position))
+        mapp.append(mapping_cat(position))
         mapp.append(temp)
 
-    #Writing the new file    
-    output_fp = mapping_fp.split(".txt")+"_with_"+field_fp+".txt"
-    write_map(output_fp,head,comm,mapp)
-    #file should be out with the new field camp added
+
+    return mapp,head,comm
+
+#convert the list from the cat file 
+#to a mapping,head format like 
+
+def parse_cat_from_list(cat_list):
+    mapping = []
+    head = None
+    for value in cat_list:
+        if head is None:
+            head = value
+        else :
+            mapping.append()
+
+
+#parse the category from a file 
+#then return mapping and header for the cat
+def parse_category_from_file(cat_fp):
+    return parse_cat_from_list(list(open(cat_fp,"U")))
+
+
+
+
+
+#=============================== Test
+
+def test_parse_cat_from_list():
+    pass
+
+def test_add_field_to_map():
+    pass
+
+
+#============================= Main Method
+def main():
+    pass
